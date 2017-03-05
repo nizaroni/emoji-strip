7ee9bd50bf3e91fc65a9645608a8359c67ad71a5
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-03-05 00:41:58 -0500

    Add failing test for skin tones
    As per issue #3.

--- tests/emoji-strip.js
+++ tests/emoji-strip.js
@@ -9,6 +9,20 @@ tape('strip emoji', function (t) {
 
   emoji = 'dealing with emoji😡 makes me feel like poop💩'
   t.equal(emojiStrip(emoji), 'dealing with emoji makes me feel like poop')
+
+  t.end()
+})
+
+tape('strip emoji with skin tones', function (t) {
+  emoji = 'skin tones🙌🏾 should be oka👌🏿y'
+  t.equal(emojiStrip(emoji), 'skin tones should be okay')
+
+  emoji = 'regardless of which tone we should be 👍🏽good'
+  t.equal(emojiStrip(emoji), 'regardless of which tone we should be good')
+
+  emoji = 'or else spock🖖🏻 will punch👊🏼 us'
+  t.equal(emojiStrip(emoji), 'or else spock will punch us')
+
   t.end()
 })
 
@@ -18,5 +32,6 @@ tape('don’t magically remove digits', function (t) {
 
   emoji = '⚡zero 0 🐗one 1 🐴two 2 🐋three 3 🎃four 4 🍌five 5 🍻six 6 💣seven 7 🍪eight 8 eight emoji ah ah ah'
   t.equal(emojiStrip(emoji), 'zero 0 one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 eight emoji ah ah ah')
+
   t.end()
 })
